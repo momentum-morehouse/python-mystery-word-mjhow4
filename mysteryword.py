712022239f8118bf2bb0ab25a2b85852eab12ba5
Marlon Howard <marl04coder@outlook.com>
2020-10-01 02:04:53 -0400

    game works great in easy mode w-no dup letters

--- mysteryword.py
+++ mysteryword.py
@@ -7,11 +7,11 @@ def play_game():
     #print("random word from list is:", random.choice(words_list))
     greeting = str(input("Welcome to Mystery Game! Choose Your Level of Difficulty. Easy: words with 4-6 characters; Normal: words with 6-8 characters; & Hard: words with 8 or more characters.  Please type Easy, Normal, or Hard: ").upper())
     if greeting == "EASY":
-        easy_list = [word for word in words_list if len(word) <= 6 and len(word) >= 4]
+        easy_list = [word for word in words_list if len(word) >= 4 and len(word) <= 6]
         random_word = random.choice(easy_list)
         #print(random_word)
     elif greeting == "NORMAL":
-        normal_list = [word for word in words_list if len(word) <= 8 and len(word) >= 6]
+        normal_list = [word for word in words_list if len(word) >= 6 and len(word) <= 8]
         random_word = random.choice(normal_list)
         #print(random_word)
     elif greeting == "HARD":
@@ -22,5 +22,62 @@ def play_game():
         print("Please type Easy, Normal, or Hard.")
 
     print("The Mystery Word contains", len(random_word), "letters.")
+    guesses_remaining = 8
+    used_letters = []
+    building_word = []
+    building_word_string = ''
+    print("You have", guesses_remaining, "guesses remaining!")
+    #comment this print line later
+    print(random_word)
+
+    while guesses_remaining > -1:
+        user_guess = str(input("Please select one letter: ").lower())
+        if user_guess in random_word.lower():
+            print(user_guess)
+            original_len = len(random_word)
+            print("_ "*original_len)
+            print(random_word.index(user_guess))
+            x = random_word.index(user_guess)
+            if user_guess in building_word_string:
+                None
+            else:
+                building_word.insert(x, user_guess)
+            print(building_word)
+            building_word_string = '' .join(building_word)
+            print(building_word_string)
+            print(random_word.lower())
+            if int(len(building_word_string)) + 1 == int(len(random_word)):
+                print("You Won!")
+                ask_user_play = str(input("Would you like to play, again? (Please enter Yes or No): ").upper())
+                if ask_user_play == "YES":
+                    play_game()
+                else:
+                    print("See ya next time!")
+                    return
+            else:
+                None 
+       
+        else:
+            if user_guess in used_letters:
+                print("You have already used this letter. Try again!")
+                guesses_remaining += 1
+            else:
+                None
+            print("Incorrect choice, please try again!")
+            guesses_remaining -= 1
+            print("You have", guesses_remaining, "guesses remaining!")
+            used_letters.append(user_guess)
+            print(used_letters)
+            if guesses_remaining == 0:
+                print("You Lose!")
+                ask_user_play = str(input("Would you like to play, again? (Please enter Yes or No): ").upper())
+                if ask_user_play == "YES":
+                    play_game()
+                else:
+                    print("See ya next time!")
+                    return
+            else:
+                None
+
 
 play_game()
